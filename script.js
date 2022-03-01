1ad67e151462302c99dca978ab07b4e59f5fd860
Marlon Macedo <marlon.s.macedo@gmail.com>
2022-03-01 19:24:56 -0300

    Create nextIndex function and maka reset Attributes of a lightOn object

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 const sinais = document.querySelectorAll([".sinal"]);
-
+let colorIndex = 0;
 
 const lightOn = {
-    'red' : () => sinais[0].setAttribute("id", "red") ,
-    'yellow' : () => sinais[1].setAttribute("id", "yellow") ,
-    'green' : () => sinais[2].setAttribute("id", "green")
-}
+  red: () => sinais[0].setAttribute("id", "red"),
+  yellow: () => sinais[1].setAttribute("id", "yellow"),
+  green: () => sinais[2].setAttribute("id", "green"),
+
+  reset: () => {
+    sinais.forEach((element) => {
+      element.removeAttribute("id");
+    });
+  },
+};
+
+const nextIndex = () => (colorIndex = colorIndex < 2 ? ++colorIndex : 0);
 
 const changeSignal = () => {
-    lightOn['red']()
-}
+  lightOn["reset"]();
+  const colors = ["red", "yellow", "green"];
+  const color = colors[colorIndex];
+  lightOn[color]();
+  nextIndex();
+};
 
-console.log(changeSignal())
+setInterval(changeSignal, 1000);
